469a6e54a48373066b1e1cb68f0aa05cce46293d
Gijs Schouten <gijswow@gmail.com>
2020-01-17 15:01:37 +0100

    credits shit

---
+++ Assets/Credits.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour {
+	private bool crawling = true;
+
+	private float speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (!crawling) return;
+
+		transform.Translate(Vector3.up * Time.deltaTime * speed);
+		if (gameObject.transform.position.y > 0) {
+			crawling = false;
+		}
+	}
+}
